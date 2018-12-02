c8da3d50d9b95d99bf1af6ae995294ee560957f5
Retr0 <Ghozt24@gmail.com>
2018-12-02 14:06:07 -0800

    alpha launch

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE burgers_db;
+
+-- insert 3 burgers
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheese', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Chicken', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Bacon', false);
